9f9c47d3ce350b6fc433f05149615e0918ddb228
JL-08 <janellelacsamana@gmail.com>
2021-04-17 14:24:11 +0800

    restyle(Activities): adjust Activities height

--- src/components/Activities/Activities.js
+++ src/components/Activities/Activities.js
@@ -8,36 +8,38 @@ import sightseeingImg from '../../images/sightseeing.jpg';
 const Activities = () => {
   return (
     <div className='activities'>
-      <div className='item-1'>
-        <img src={bikingImg} alt='biking' />
-        <div>
-          <span>01</span>
-          <h2>BIKING</h2>
-          <i class='fas fa-arrow-right'></i>
+      <div className='grid-container'>
+        <div className='item-1'>
+          <img src={bikingImg} alt='biking' />
+          <div>
+            <span>01</span>
+            <h2>BIKING</h2>
+            <i class='fas fa-arrow-right'></i>
+          </div>
         </div>
-      </div>
-      <div className='item-2'>
-        <img src={swimmingImg} alt='swimming' />
-        <div>
-          <span>02</span>
-          <h2>SWIMMING</h2>
-          <i class='fas fa-arrow-right'></i>
+        <div className='item-2'>
+          <img src={swimmingImg} alt='swimming' />
+          <div>
+            <span>02</span>
+            <h2>SWIMMING</h2>
+            <i class='fas fa-arrow-right'></i>
+          </div>
         </div>
-      </div>
-      <div className='item-3'>
-        <img src={hikingImg} alt='hiking' />
-        <div>
-          <span>03</span>
-          <h2>HIKING</h2>
-          <i class='fas fa-arrow-right'></i>
+        <div className='item-3'>
+          <img src={hikingImg} alt='hiking' />
+          <div>
+            <span>03</span>
+            <h2>HIKING</h2>
+            <i class='fas fa-arrow-right'></i>
+          </div>
         </div>
-      </div>
-      <div className='item-4'>
-        <img src={sightseeingImg} alt='sighseeing' />
-        <div>
-          <span>04</span>
-          <h2>SIGHTSEEING</h2>
-          <i class='fas fa-arrow-right'></i>
+        <div className='item-4'>
+          <img src={sightseeingImg} alt='sighseeing' />
+          <div>
+            <span>04</span>
+            <h2>SIGHTSEEING</h2>
+            <i class='fas fa-arrow-right'></i>
+          </div>
         </div>
       </div>
     </div>
