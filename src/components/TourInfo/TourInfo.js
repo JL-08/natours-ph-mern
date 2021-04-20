d625f3bfc238b91ef7af02c7aa0ac803b59dbccf
JL-08 <janellelacsamana@gmail.com>
2021-04-21 00:12:56 +0800

    Add TourInfo component | restyle(TourBanner): fix width where it exceeds the viewport's width

---
+++ src/components/TourInfo/TourInfo.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import './TourInfo.scss';
+import sample from '../../images/swimming.jpg';
+
+const TourInfo = () => {
+  return (
+    <div className='tour-info'>
+      <div className='tour-stats'>
+        <div className='tour-facts'>
+          <h2>QUICK FACTS</h2>
+          <div className='facts-wrapper'>
+            <ul className='facts-icon'>
+              <li>
+                <i class='fas fa-calendar'></i>
+              </li>
+              <li>
+                <i class='fas fa-chart-line'></i>
+              </li>
+              <li>
+                <i class='fas fa-users'></i>
+              </li>
+              <li>
+                <i class='fas fa-star'></i>
+              </li>
+            </ul>
+
+            <ul className='facts-text'>
+              <li>
+                <h4>
+                  NEXT DATE <span>June 2021</span>
+                </h4>
+              </li>
+              <li>
+                <h4>
+                  DIFFICULTY <span>Medium</span>
+                </h4>
+              </li>
+              <li>
+                <h4>
+                  PARTICIPANTS <span>15 People</span>
+                </h4>
+              </li>
+              <li>
+                <h4>
+                  RATING <span>4.8/5</span>
+                </h4>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className='tour-guides'>
+          <h2>YOUR TOUR GUIDES</h2>
+          <div className='guides-wrapper'>
+            <ul>
+              <li>
+                <div className='tour-guide-avatar'>
+                  <img src={sample} alt='sample avatar' />
+                </div>
+                <div className='tour-guide-info'>
+                  <h4>LEAD GUIDE</h4>
+                  <span>Jennifer Hardy</span>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className='tour-summary'>
+        <h2>ABOUT BAGUIO RAILING FOREST</h2>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio
+          facilis, corrupti recusandae sequi, earum enim repudiandae quisquam
+          deleniti minus nesciunt quia molestiae. Pariatur dicta minus, enim,
+          tempore quam nihil error aliquid deleniti saepe est aspernatur numquam
+          nobis necessitatibus fuga quas modi autem. Ut vel placeat dolorum
+          vitae adipisci facilis repudiandae eaque, magnam inventore officiis
+          similique modi quos odit ullam, laboriosam qui dolores, deleniti
+          veritatis iure velit quasi esse? Perferendis consequuntur optio
+          molestias omnis accusamus eveniet labore autem blanditiis, corrupti
+          enim. Corrupti dolorem tempora mollitia magni omnis qui ipsa magnam,
+          sapiente necessitatibus voluptatum tempore laudantium, ab porro, id
+          soluta! Asperiores, aliquid?
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default TourInfo;
--- src/components/TourPage/TourPage.js
+++ src/components/TourPage/TourPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import TourBanner from '../TourBanner/TourBanner';
+import TourInfo from '../TourInfo/TourInfo';
 
 const TourPage = () => {
   return (
     <div className='tour-page'>
       <TourBanner />
+      <TourInfo />
     </div>
   );
 };
