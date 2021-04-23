270ff4439857c2fdb4bec27ee0476375d510960a
JL-08 <janellelacsamana@gmail.com>
2021-04-24 01:46:59 +0800

    Add logged in user in nav bar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NavBar.scss';
 import navLogo from '../../images/logo-white.png';
+import sample from '../../images/swimming.jpg';
 
 const NavBar = () => {
+  const [user, setUser] = useState({ fname: 'John', lname: 'Smith' });
+
+  const UserNav = () => {
+    if (user) {
+      return (
+        <>
+          <div className='nav-profile'>
+            <img src={sample} alt='' id='nav-avatar' />
+            <h4>
+              {user.fname} {user.lname}
+            </h4>
+          </div>
+          <button>LOGOUT</button>
+        </>
+      );
+    }
+
+    return (
+      <>
+        <button>LOGIN</button>
+        <button className='nav__btn-primary'>SIGNUP</button>
+      </>
+    );
+  };
+
   return (
     <nav className='nav-bar'>
       <div>
         <img src={navLogo} alt='white natours logo' />
       </div>
       <div>
-        <button>LOGIN</button>
-        <button className='nav__btn-primary'>SIGNUP</button>
+        <UserNav />
       </div>
     </nav>
   );
