a4e8817c26dc44d5e7d3d57084a4f52c539566e1
Janelle <janellelacsamana@gmail.com>
2022-08-28 15:00:57 +0800

    refactor: use cleanup

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,26 +1,35 @@
 import { useEffect, useState } from 'react';
 import { statuses } from '../constants/requestStatuses';
 
-export const useFetch = (apiFn) => {
+export const useFetch = (apiFn, options = '') => {
   const [status, setStatus] = useState(statuses.IDLE);
   const [data, setData] = useState([]);
 
   useEffect(() => {
+    let isRendered = true;
+
     if (!apiFn) return;
     const fetchData = async () => {
       setStatus(statuses.LOADING);
 
       try {
-        const response = await apiFn();
-        setData(response.data);
-        setStatus(statuses.SUCCESS);
+        const response = await apiFn(options);
+
+        if (isRendered) {
+          setData(response.data);
+          setStatus(statuses.SUCCESS);
+        }
       } catch (error) {
         setStatus(statuses.FAILED);
       }
     };
 
     fetchData();
-  }, [apiFn]);
 
-  return { status, data };
+    return () => {
+      isRendered = false;
+    };
+  }, [apiFn, options]);
+
+  return { data, status };
 };
